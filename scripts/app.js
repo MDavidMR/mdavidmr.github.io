919d8fd41fe5a1b83cef71ec5319aa703232e31e
mmorales-devit <83131485+mmorales-devit@users.noreply.github.com>
2021-05-03 07:02:01 -0500

    Temp 1

--- scripts/app.js
+++ scripts/app.js
@@ -5,34 +5,88 @@ class App {
 	};
 	session;
 	step;
-	data = {};
+	data = {
+		terms: {
+			accepted: false,
+		},
+	};
 	location = { latitude: 0, longitude: 0 };
 }
 
-var app = new App();
+let appObj = new App();
+let session;
+let onBoarding;
 
 $(document).ready(function () {
-	app.step = 1;
+	appObj.step = 1;
 	$("button.btn-step").click(btnStep_Click);
-	goToStep(app.step);
+	$("#btnGetLocation").click(btnGetLocation_Click);
+	goToStep(appObj.step);
 	getTerms();
 });
 
-function btnStep_Click(e) {
-	var step = $(e.currentTarget).data("step");
-	if (!!step) goToStep(step);
+async function app() {
+	onBoarding = createOnBoarding();
+	await onBoarding.warmup();
+	session = await createSession();
 }
 
-function getGeo() {}
+function btnGetLocation_Click() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getCurrentPosition);
+	} else {
+		alert("Geolocation is not supported by this browser.");
+	}
+}
+
+function getCurrentPosition(position) {
+	const latitude1 = position.coords.latitude;
+	const longitude1 = position.coords.longitude;
+
+	var resultgeo = onBoarding
+		.addGeolocation({
+			token: session,
+			latitude: latitude1,
+			longitude: longitude1,
+		})
+		.then((res) => showLocation(res.location));
+}
+
+function showLocation(location) {
+	$('.current-location').html(location)
+}
+
+function btnStep_Click(e) {
+	var step = $(e.currentTarget).data("step");
+	var action = $(e.currentTarget).data("action");
+	performAction(action);
+	if (!!step) goToStep(step);
+}
 
 function goToStep(step) {
 	$("#frmData .container").find(".step.shown").removeClass("shown");
 	$(`#frmData .container #dvStep${step}`).addClass("shown");
-	app.step = step;
+	appObj.step = step;
+}
+
+function performAction(action) {
+	switch (action) {
+		case "accept-terms":
+			appObj.data.terms.accepted = true;
+	}
 }
 
 function getTerms() {
 	$.getJSON("scripts/terms.json", function (data) {
 		$("#dvStep1 .card-body .content").html(data.data);
+	});
+}
+
+//////////// OnBoarding
+function createOnBoarding() {
+	return window.OnBoarding.create({
+		apiKey: appObj.incode.apiURL,
+		apiURL: appObj.incode.apiKey,
+		lang: "en-US",
 	});
 }
